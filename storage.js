f3411a41c0fe001290714f4db0bd6298627d1c2b
venthan002 <neethiventhan002@gmail.com>
2025-10-31 13:53:04 +0530

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,9 @@
+// Save and retrieve a value in localStorage
+localStorage.setItem("name", "neethi");
+let localValue = localStorage.getItem("name");
+console.log("Local Storage Value:", localValue);
+
+// Save and retrieve a value in sessionStorage
+sessionStorage.setItem("course", "Web Development");
+let sessionValue = sessionStorage.getItem("course");
+console.log("Session Storage Value:", sessionValue);
